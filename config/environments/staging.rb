6a2a222a64fb568bb0e75c8b990e50dca5df7e37
Yasfi Fauzie <yasfi.mail@gmail.com>
2017-12-12 22:47:22 +0700

    add conf

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -80,10 +80,10 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-   :address              => "smtp.gmail.com",
-   :port                 => 587,
-   :user_name            => ENV['GMAIL_USERNAME'],
-   :password             => ENV['GMAIL_PASSWORD'],
+   :address              => "mail.kemdikbud.go.id",
+   :port                 => 465,
+   :user_name            => ENV['APLP_MAILER_USERNAME'],
+   :password             => ENV['APLP_MAILER_PASSWORD'],
    :authentication       => "plain",
   :enable_starttls_auto => true
   }
