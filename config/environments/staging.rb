33ca23bf8714db9644dd2ef2209d3bc7c8eff0ec
Yasfi Fauzie <yasfi.mail@gmail.com>
2017-12-21 07:18:05 +0700

    server static

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -62,7 +62,8 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "aplp_#{Rails.env}"
   config.action_mailer.perform_caching = false
-
+  config.serve_static_assets = true
+  
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
@@ -87,6 +88,7 @@ Rails.application.configure do
    :authentication       => "plain",
   :enable_starttls_auto => true
   }
+  
   # config.action_mailer.smtp_settings = {
   #  :address              => "mail.kemdikbud.go.id",
   #  # :address              => "smtp.kemdikbud.go.id",
