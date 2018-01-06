655dfd176a03f13733ef6b5eff924fe302a03c6f
Yasfi Fauzie <yasfi.mail@gmail.com>
2018-01-07 06:22:01 +0700

    updated env requirement

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
+if Rails.env.production?
+	SIZE = 5
+	kbri = 'KBRI.xlsx'
+else
+	kbri = 'KBRI-development.xlsx'
+	SIZE = 30
+end
+
 	puts 'start production data behaviour'
 
 	# NEGARA
@@ -16,7 +24,7 @@
 
 	# puts 'start create admin consulate seed'
 	#Validate by username
-	kbrixls = Roo::Excelx.new( Rails.root.join('KBRI-development.xlsx') )
+	kbrixls = Roo::Excelx.new( Rails.root.join(kbri) )
 
 	kbrixls.each_row_streaming(offset: 1) do |row|
 		admin = Admin.new
@@ -65,24 +73,25 @@
 
 	if Admin.where(role:"admin").count < 1
 		adm = Admin.new
-		adm.username = ENV["APLP_ADMIN"]
-		adm.password = ENV["APLP_PASSWORD"]
-		adm.password_confirmation = ENV["APLP_PASSWORD"]
+		adm.username = ENV["BIPA_ADMIN"]
+		adm.password = ENV["ADMIN_PASSWORD"]
+		adm.password_confirmation = ENV["ADMIN_PASSWORD"]
 		adm.name = "Admin Pusat"
 		adm.role = "admin"
 		adm.save
 	end
 
 	if Landing.count < 1
-		Landing.find_or_create_by(title:"Selamat datang di aplikasi APLP 2018 <br> Atdikud BPKLN Kemendikbud RI")
+		Landing.find_or_create_by(title:"Selamat datang di aplikasi BIPA 2018 <br> Atdikud BPKLN Kemendikbud RI")
 	end
 
 puts 'end start production data behaviour'
 
-unless Rails.env.production?
+
+# unless Rails.env.production?
 	puts 'start development data behaviour'
-	if User.count < 30
-		30.times do |index|
+	if User.count < SIZE
+		SIZE.times do |index|
 			index=index+1
 			u = User.create( email:"user#{index}@mail.com", password:"user#{index}@mail.com", password_confirmation:"user#{index}@mail.com", name:"user#{index}",
 									passport:"0890#{index}092", passport_expire:Time.now.next_month, gender:["lk",'pr'].sample, country_id:[1,2,3].sample, country_live_id:[1,2,3].sample,
@@ -92,4 +101,4 @@ unless Rails.env.production?
 		end
 	end
 	puts 'end start development data behaviour'
-end
+# end
